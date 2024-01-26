f67d7cced9bddbb90666a694d4d4606264a514e4
Hajdu Dávid <hdavid998@gmail.com>
2024-01-27 00:56:42 +0100

    redmine_cli updated

--- src/redmine_cli/cli.py
+++ src/redmine_cli/cli.py
@@ -7,5 +7,9 @@ def main():
 	config = RedmineConfig()
 	url, api_key = config.get_user_info()
 	redmine_api = RedmineApi({"url": url, "api_key": api_key})
-	redmine_api.get_issues({"user_id": "me"})
+	# redmine_api.get_issue_by_id(id=10469)
+	# redmine_api.get_time_entry_by_id(42413)
+	redmine_api.get_time_entries({"user_id": "me", "limit": 5})
+	redmine_api.get_issues({"user_id": "me", "limit": 5})
+	
 
